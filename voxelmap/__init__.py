4ad6896842fc554d2bc2bbe1b9ec4b97482dd4d9
Andrew Garcia <garcia.gtr@gmail.com>
2025-09-26 18:48:48 -0500

    simplify codebase, modularize it , make larger dependencies optional in a fuller install

--- voxelmap/__init__.py
+++ voxelmap/__init__.py
@@ -1,3 +1,13 @@
-from voxelmap.legacy import *
-from voxelmap.annex import *
-from voxelmap.main import *
+from voxelmap.core import Model, binarize
+from voxelmap.io import (
+    save_array, load_array,
+    tojson, load_from_json,
+    toTXT, objcast
+)
+
+# Mesh extras optional
+try:
+    from voxelmap.mesh import MarchingMesh, MeshView, ImageMap, ImageMesh
+except ImportError:
+    pass
+
