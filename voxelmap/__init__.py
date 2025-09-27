a9c9075c6648c70a481501bee1372d6641362c28
Andrew Garcia <garcia.gtr@gmail.com>
2025-09-27 12:39:04 -0500

    for rtd use all to tell sphinx to expose what exists

--- voxelmap/__init__.py
+++ voxelmap/__init__.py
@@ -5,8 +5,16 @@ from voxelmap.io import (
     toTXT, objcast
 )
 
+__all__ = [
+    "Model", "binarize",
+    "save_array", "load_array",
+    "tojson", "load_from_json",
+    "toTXT", "objcast",
+]
+
 # Mesh extras optional
 try:
     from voxelmap.mesh import MarchingMesh, MeshView, ImageMap, ImageMesh
+    __all__ += ["MarchingMesh", "MeshView", "ImageMap", "ImageMesh"]
 except ImportError:
-    MarchingMesh = MeshView = ImageMap = ImageMesh = None
+    pass
